43f50a00e33cf339e2b76ab4d128d55e0f423a40
Nguy3n <nguy3n_@hotmail.com>
2015-07-07 00:08:07 +0200

    Får noen feil før den er kompilerbar.

---
+++ Documents/DemoScreens/app/src/main/java/com/liferay/demoscreens/UserPortraitActivity.java
@@ -0,0 +1,54 @@
+package com.liferay.demoscreens;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class UserPortraitActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_portrait);
+
+        findViewById(R.id.new_issue).setOnClickListener(this);
+        findViewById(R.id.list_issue).setOnClickListener(this);
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_portrait, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.new_issue) {
+            startActivity(new Intent(this, DDLFormActivity.class));
+        } else {
+            startActivity(new Intent(this, DDLListActivity.class));
+        }
+
+
+    }
+}
